95fdceaf02b61b0cd77bf3d258aa7a9a28738aa5
Andrew Breyen <AndrewBreyen@users.noreply.github.com>
2016-05-14 21:16:34 -0500

    Add files via upload

---
+++ RUN-ME-FIRST.sh
@@ -0,0 +1,14 @@
+#BIN-BASH
+
+pause(){
+ read -n1 -rsp $'Press any key to continue or Ctrl+C to exit...\n'
+}
+
+sudo apt-get update
+sudo apt-get install fswebcam
+sudo apt-get install cron
+sudo apt-get update
+
+clear
+echo Initial Installs complete. You can scroll up to see results.
+read -n 1 -p "Press any key to continue..."
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Raspberry Pi Time Lapse Scripts
+
+See the [Wiki Documentation](https://github.com/AndrewBreyen/rPiTimeLapse/wiki "Wiki-page") for directions about how to use these scripts.
